737ca0fdf8799519f57acf076d79146d6ce61153
Anand Singh <anand.krs@gmail.com>
2011-09-24 02:04:10 +0530

    with correct Algo

--- sevseg.c
+++ sevseg.c
@@ -342,14 +342,6 @@ void find_nxt_ptr(int first, int sec,int pos,int *ptr, int inv)
 void chk_ptrn(int *led)
 {
 
-  if((led[0] == 0) && (led[1] == 0) && (led[2] == 0) && (led[3] == 0) && (led[4] == 0) && (led[5] == 0) && (led[6] == 0))
- {
-d =1;
-  } 
-  if(d == 1)
-{
- printf("+++++++++++++++++++++++++++++++++++++++++++\n");
-}
    //check if that pattern already exist in memory pattern
    if( 0 == chk_mem_ptrn(led))
    {
@@ -361,218 +353,74 @@ d =1;
       save_mem_ptrn(led); //if no then save it as memory pattern
        //display_led(led[0], led[1], led[2], led[3], led[4], led[5], led[6]);
    }
-if(d == 1)
-{
- printf("+++++++++++++++++++++++++++++++++++++++++++\n");
- d =0;
-}
 }
 
-int learningProcess(int idx, int inv)
+int learningProcess()
 {
   int round = 0;
   int firstcycle = 0;
   int secondcycle = 0;
   int ii = 0;
   int led[MAX_LED] = { 0,0,0,0,0,0,0 };
+  int temp[MAX_LED] = { 0,0,0,0,0,0,0 };
   // clear any previous display
+  int curr_seed_idx = 0;
+  int mem_idx =0;
   clr();
 
-  secondcycle = (MAX_LED - (1 + idx));
-  round = (MAX_LED - (1 + idx));
-  
-  
-  //clear seed patern
-  for(int i=0;i<MAX_LED; i++)
+  chk_ptrn(led);
+
+  while(curr_seed_idx < MAX_LED)
   {
-	if(inv == 0)
-	{
-	seed_ptr[i] = 0;
-	}else
-	{
-	seed_ptr[i] = 1;
-	}
+  	  //prepare the seed pattern
+	  printf("-----%d------\n",curr_seed_idx);
+	  for(int i = 0; i<MAX_LED; i++)
+	  {
+	    seed_ptr[i] = ledsys[curr_seed_idx][i];
+	    printf("%d ",seed_ptr[i]);
+	  }
+	  printf("\n");
+	  curr_seed_idx++;
+
+  	  secondcycle = ipat;
+          mem_idx = 0;
+	  while(secondcycle > 0)
+	  {
+                printf("\t [%d %d]: ", secondcycle, mem_idx);
+	  	for(int i = 0; i<MAX_LED; i++)
+	  	{
+			led[i] =  mem_ptrn[mem_idx][i] | seed_ptr[i];
+			printf("%d ",led[i]);
+		}
+	
+                printf("\n");
+		// check pattern
+	 	chk_ptrn(led);
+                secondcycle--;
+		mem_idx++;
+	  }
   }
-  //prepare the seed pattern
-  for(int i = 0; i<=idx; i++)
-  {
-    if(inv == 0)
-    {
-    seed_ptr[0] |= ledsys[i][0];
-    seed_ptr[1] |= ledsys[i][1];
-    seed_ptr[2] |= ledsys[i][2];
-    seed_ptr[3] |= ledsys[i][3];
-    seed_ptr[4] |= ledsys[i][4];
-    seed_ptr[5] |= ledsys[i][5];
-    seed_ptr[6] |= ledsys[i][6];
-    }
-    else{
-    seed_ptr[0] &= ~ledsys[i][0];
-    seed_ptr[1] &= ~ledsys[i][1];
-    seed_ptr[2] &= ~ledsys[i][2];
-    seed_ptr[3] &= ~ledsys[i][3];
-    seed_ptr[4] &= ~ledsys[i][4];
-    seed_ptr[5] &= ~ledsys[i][5];
-    seed_ptr[6] &= ~ledsys[i][6];
-    }
-  }
-  printf("\t");
-      for(int i =0;i<MAX_LED; i++)
-      {
-        printf("%d ",seed_ptr[i]);
-      }
-      printf("\n");
-  // check seed pattern
-  chk_ptrn(seed_ptr);
-
-    for(int i = 0; i<MAX_LED; i++)
-    {
-      if(inv ==0)
-      {
-       mov_ptrn[i] = 0;
-      }
-      else{
-	mov_ptrn[i] = 1;
-      } 
-    }
-
-  while(firstcycle < round)
-  {
-    ii = 0;
-    printf(">>>>%d %d %d\n",idx, firstcycle,secondcycle);
-    //printf(firstcycle);
-    //printf(" ");
-    //printf(secondcycle);
-    //clear the moving pattern
-
-    // create the moving pattren
-    for(int i = 0; i<=firstcycle;i++)
-    {
-      if(inv == 0)
-      {
-      mov_ptrn[(idx+1) + firstcycle] = 1;
-      }else
-      {
-      mov_ptrn[(idx+1) + firstcycle] = 0;
-      } 
-    }
-   
-    //printf("\t\t");
-    // for(int i =0;i<MAX_LED; i++)
-    //  {
-    //    printf("%d ", mov_ptrn[i]);
-    //  }
-    
-    while(ii < secondcycle)
-    {
-
-      printf("\t----\n");
-      printf("\t %d %d %d\n", ii,firstcycle,secondcycle);
-
-      find_nxt_ptr(firstcycle, secondcycle, ii, led, inv);
-      ii++;
-      printf("\t");
-      for(int i =0;i<MAX_LED; i++)
-      {
-        printf("%d ",led[i]);
-      }
-      printf("\n");
-      
-      chk_ptrn(led);
-      
-    }
-    firstcycle++;
-    secondcycle--;
-  }
-
-
 
   return 0;
 }
 
 void loop()
 {
-for(int i =0 ;i<MAX_PATTERN;i++)
-{
-for(int j =0 ;j<MAX_LED;j++)
-{
-mem_ptrn[i][j] = 0;
-lrn_ptrn[i][j] = 0;
-}
-}
+	for(int i =0 ;i<MAX_PATTERN;i++)
+	{
+		for(int j =0 ;j<MAX_LED;j++)
+		{
+			mem_ptrn[i][j] = 0;
+			lrn_ptrn[i][j] = 0;
+		}
+	}
 
- //for(int i=0; i<MAX_LED; i++)
- //{
-#if 1
-printf("----------------0----------------\n");
-learningProcess(0,0);
-printf("----------------1----------------\n");
-learningProcess(1,0);
-printf("----------------2----------------\n");
-learningProcess(2,0);
-printf("----------------3----------------\n");
-learningProcess(3,0);
-printf("----------------4----------------\n");
- learningProcess(4,0);
-printf("----------------5----------------\n");
-learningProcess(5,0);
-printf("----------------6----------------\n");
-learningProcess(6,0);
- printf("Dump Lrn\n");
- dump_lrn_ptrn();
- printf("Dump mem\n");
- dump_mem_ptrn();
-#endif
-printf("----------------0----------------\n");
-learningProcess(0,1);
-printf("----------------1----------------\n");
-learningProcess(1,1);
-printf("----------------2----------------\n");
-learningProcess(2,1);
-printf("----------------3----------------\n");
-learningProcess(3,1);
-printf("----------------4----------------\n");
- learningProcess(4,1);
-printf("----------------5----------------\n");
-learningProcess(5,1);
-printf("----------------6----------------\n");
-learningProcess(6,1);
- //}
- //printf("\n\n");
- printf("Dump Lrn\n");
- dump_lrn_ptrn();
- printf("Dump mem\n");
- dump_mem_ptrn();
-
-#if 0
-  // 7 step learning process
-  for(int i=0; i<MAX_LED; i++)
-  {
-    printf("%d.",i);
-    //printf(i);
-    learned = learningProcess(i);
-    if(learned == 1)
-      break;
-    //delay(500);
-    printf("\n\n");
-    printf("Dump Lrn\n");
-    dump_lrn_ptrn();
-    printf("Dump mem\n");
-    dump_mem_ptrn();
-  }
-  //printf(" ==> ");
-  //printf(ipat);
-  //dump_pattern();
-  //}
-
-
-  //printf("Starting\n");
-  for(int i=0; i<=9; i++)
-  {
-    LedSeg(i);
-    delay(250);
-  }
-#endif
+	printf("--------------------------------\n");
+	learningProcess();
+	printf("Dump Lrn\n");
+	dump_lrn_ptrn();
+	printf("Dump mem\n");
+	dump_mem_ptrn();
 
 }
 
